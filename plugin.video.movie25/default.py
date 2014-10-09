a83437a33fd89389b9c000da508e2deb5e940e75
ykhehra1 <ykhehra1@hotmail.com>
2014-10-09 11:55:46 -0600

    u

--- plugin.video.movie25/default.py
+++ plugin.video.movie25/default.py
@@ -98,9 +98,9 @@ def MAIN():
         elif index==27:
             main.addDirHome('The New Pirate Bay','https://raw.github.com/mash2k3/MashUpTNPB/master/TNPB_Directory.xml',235,art+'/tnpb.png')
         elif index==29:
-            main.addDirHome('Staael 1982','https://raw.github.com/Coolstreams/bobbyelvis/master/Staael_Directory.xml',235,art+'/staael2014.png')
+            main.addDirHome('Staael 1982','https://raw.github.com/Coolstreams/bobbyelvis/master/Staael_Directory.xml',236,art+'/staael2014.png')
         elif index==34:
-            main.addDirHome('Demon88 Movies','https://raw.github.com/mash2k3/demon88/master/Demon88_Directory.xml',235,art+'/demon88.png')
+            main.addDirHome('Demon88 Movies','https://raw.github.com/mash2k3/demon88/master/Demon88_Directory.xml',237,art+'/demon88.png')
         elif index==37:
             main.addDirHome('ONE242415','https://raw.github.com/mash2k3/One242415/master/One242415_Directory.xml',235,art+'/one252515.png')
         elif index==31:
